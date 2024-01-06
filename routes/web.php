1d65b10c03c41daceb2611e11699768041587e17
EC2 Default User <ec2-user@ip-172-31-29-205.ap-northeast-1.compute.internal>
2024-01-06 07:30:37 +0000

    Lalavel4

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\NewsController;
+use App\Http\Controllers\Admin\ProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// NewsControllerのルート設定
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+// ProfileControllerのルート設定
+Route::controller(ProfileController::class)->prefix('admin/profile')->group(function() {
+    Route::get('create', 'add');
+    Route::get('edit', 'edit');
+});
